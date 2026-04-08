e79d06dce8d967e2a9975e52e22214b5d54d320c
furkancelik1 <furkansteam2022@gmail.com>
2026-04-08 21:08:42 +0300

    edited ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/db";
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
